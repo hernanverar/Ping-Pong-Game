fcff9ccdb091204ba9617dbc59083f125657d6b1
hernan verar <hernanverar@gmail.com>
2023-05-02 16:33:51 -0700

    This add files created for our project

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using PingPong.Models;
+
+namespace PongGame
+{
+  class Program
+  {
+  
+    static void Main()
+
+    {
+      Console.WriteLine("ping");
+      Console.WriteLine("pong");
+      string [] stringNumber1 = (3); 
+      string [] stringNumber = (5); 
+    }
+  }
+}
---
+++ UserInterfaceModels/Banners.cs
@@ -0,0 +1,17 @@
+namespace ShapeTracker.UserInterfaceModels
+{
+  class WelcomeBanner
+  {
+    public static string Welcome { get; } = @" ";
+  }
+
+  class GoodbyeBanner
+  {
+    // GoodbyeBanner code here.
+  }
+
+  class SubMenuBanner
+  {
+    // SubMenuBanner code here.
+  }
+}
